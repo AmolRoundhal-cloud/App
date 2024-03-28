95fcd2082c5d265c4fde7ccd29051c23638b437b
AmolRoundhal-cloud <68346371+AmolRoundhal-cloud@users.noreply.github.com>
2024-03-28 10:23:14 +0530

    Delete IMasterFloorDetailsRepository.java

--- IMasterFloorDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ats.emami.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ats.emami.entity.MasterFloorDetailsEntity;
-
-public interface IMasterFloorDetailsRepository extends JpaRepository<MasterFloorDetailsEntity, Integer> {
-
-}
